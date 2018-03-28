79cc5c34f2b6d843ab0886688ac07d8a208476cd
Mateusz Szymański <mateusz.szymanski@hotmail.com>
2018-03-28 18:09:27 +0200

    About page updated.

--- src/Client.Vue/src/views/About.vue
+++ src/Client.Vue/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page!!</h1>
+    <h1>This is an application created for technical interview</h1>
+    <h2><a href="mailto:mateusz.jakub.szymanski@gmail.com">Mateusz Szymański</a></h2>
   </div>
 </template>
