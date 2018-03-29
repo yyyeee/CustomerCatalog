dc2d8b86aa748c32df413b918ed7dbd2826d7445
Mateusz Szymański <mateusz.szymanski@hotmail.com>
2018-03-29 22:41:53 +0200

    Displaying, sorting, filtering customers added

--- src/Client.Vue/src/components/CustomerList.vue
+++ src/Client.Vue/src/components/CustomerList.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="hello">
-    <h3>Customers</h3>
-    <ul>
-      <li v-for="item in items">
-        {{ item.name }}
-      </li>
-    </ul>
+    <vue-good-table
+      title="Customers"
+      :columns="columns"
+      :rows="items"
+      :paginate="true"/>
   </div>
 </template>
 
@@ -16,15 +15,48 @@ import { CustomerDto } from '@/services';
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private items: CustomerDto[];
+  
+  private columns = [
+    {
+      label: 'Id',
+      field: 'id'
+    },
+    {
+      label: 'Name',
+      field: 'name',
+      filterable: true
+    },
+    {
+      label: 'Created On',
+      field: 'creationTime',
+      type: 'date',
+      inputFormat: 'YYYY-MM-DD',
+      outputFormat: 'MMM Do YY',
+      sortable: true
+    },
+    {
+      label: 'Status',
+      field: 'status',
+      filterable: true,
+      filterDropdown: true,
+      formatFn: function(value: number) {
+        switch (value) {
+          case 1:
+            return 'Prospective';
+          case 2:
+            return 'Current';
+          case 3:
+            return 'Non-active';
+          default:
+            return 'Unknown';
+        }
+      },
+      filterOptions: [  
+            { value: '1', text: 'Prospective' },  
+            { value: '2', text: 'Current' },  
+            { value: '3', text: 'Non-active' }  
+          ],
+    }
+  ];
 }
 </script>
-
-<style scoped lang="scss">
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/Client.Vue/src/main.ts
+++ src/Client.Vue/src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueGoodTablePlugin from 'vue-good-table';
+
+Vue.use(VueGoodTablePlugin);
 
 Vue.config.productionTip = false;
 
